71037558df83faac4edea5ba69d3a346edbc4c2d
GitHub Action <action@github.com>
2020-08-29 08:33:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/bpzj/autodeploy/MyBundle.kt
+++ src/main/kotlin/com/github/bpzj/autodeploy/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.bpzj.autodeploy
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/bpzj/autodeploy/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/bpzj/autodeploy/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.bpzj.autodeploy.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bpzj.autodeploy.MyBundle
 
 class MyApplicationService {
 
